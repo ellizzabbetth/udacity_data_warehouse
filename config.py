fe92697a18c88e0dd8183c07cad9b51f06bc6031
eli <ellizzabbetth@gmail.com>
2024-01-16 15:21:31 -0500

    final

--- config.py
+++ config.py
@@ -3,13 +3,12 @@ import os
 
 from dotenv import load_dotenv
 
-
 ROOT_PATH = os.path.dirname(__file__)
-
 
 load_dotenv(os.path.join(ROOT_PATH, ".env"))
 CONFIG = configparser.ConfigParser()
 CONFIG.read(os.path.join(ROOT_PATH, "dwh.cfg"))
+
 # CONFIG.add_section("AWS")
 CONFIG.set("AWS", "KEY", os.environ.get("AWS_KEY"))
 CONFIG.set("AWS", "SECRET", os.environ.get("AWS_SECRET"))
